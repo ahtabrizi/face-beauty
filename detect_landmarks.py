592043043647b0018dbcad7e3612b75ea88bcbaa
redhood <a.tabrizi.mech@gmail.com>
2020-03-24 15:08:17 +0430

    merged left and right landmarks

--- detect_landmarks.py
+++ detect_landmarks.py
@@ -7,35 +7,35 @@ from transforms import *
 # Run the 3D face alignment on a test image, without CUDA.
 FA = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cpu', flip_input=False)
 
-left_img = io.imread('post/Case#1_3_L_post_Pa_Ro.png')
-right_img = io.imread('post/Case#1_2_R_post_Pa_Ro.png')
-front_img = io.imread('post/Case#1_1_F_post_Pa_Ro_Sc_Tr_Ep.png')
+left_img = io.imread('post/Case#2_3_L_post_Pa_Ro.png')
+right_img = io.imread('post/Case#2_2_R_post_Pa_Ro.png')
+front_img = io.imread('post/Case#2_1_F_post_Pa_Ro_Sc_Tr_Ep.png')
 
 left_preds = np.asarray(FA.get_landmarks(left_img)[-1])
 right_preds = np.asarray(FA.get_landmarks(right_img)[-1])
 front_preds =  np.asarray(FA.get_landmarks(front_img)[-1])    
 
 print("PLOTTING")
-fig = plt.figure()
-ax = plt.gca()
-drawLandmarksOnFace(ax, left_img, left_preds, color=(1,0,0))
-fig = plt.figure()
-ax = plt.gca()
-drawLandmarksOnFace(ax, right_img, right_preds, color=(1,0,0))
-
+#fig = plt.figure()
+#ax = plt.gca()
+#drawLandmarksOnFace(ax, left_img, left_preds, color=(1,0,0))
+#fig = plt.figure()
+#ax = plt.gca()
+#drawLandmarksOnFace(ax, right_img, right_preds, color=(1,0,0))
+#
 right_preds = flip(right_preds, right_img.shape[1], 0)
 right_preds = coincideLandmarkLR(left_preds, right_preds)
-
-fig = plt.figure()
-ax = plt.axes()
-drawLandmarks2D(ax, left_preds, color=(0,0,1))
-drawLandmarks2D(ax, right_preds, color=(1,0,0))
-#i = 27
-#ax.plot(left_preds[i, 0], left_preds[i, 1],color=(0,1,0), **plot_style)
-ax.set_xlim([0, right_img.shape[1]])
-ax.set_ylim([0, right_img.shape[0]])
-ax.set_aspect('equal')
-ax.invert_yaxis()
+#
+#fig = plt.figure()
+#ax = plt.axes()
+#drawLandmarks2D(ax, left_preds, color=(0,0,1))
+#drawLandmarks2D(ax, right_preds, color=(1,0,0))
+##i = 27
+##ax.plot(left_preds[i, 0], left_preds[i, 1],color=(0,1,0), **plot_style)
+#ax.set_xlim([0, right_img.shape[1]])
+#ax.set_ylim([0, right_img.shape[0]])
+#ax.set_aspect('equal')
+#ax.invert_yaxis()
 
 # swap x and z axes to be same with frontal axes
 left_preds[:, [0, 2]] = left_preds[:, [2, 0]]
@@ -44,11 +44,18 @@ right_preds[:, [0, 2]] = right_preds[:, [2, 0]]
 front_preds = flip(front_preds, np.max(front_preds[:,2]), 2)
 front_preds = coincideLandmarkFP(front_preds, left_preds)
 
+P = mergeLR(left_preds, right_preds)
+
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 drawLandmarks3D(ax, left_preds, label="Left")
 drawLandmarks3D(ax, right_preds, label="Right", cPoint=(1,0,0), cLine=(0.7,0.2,0.2))
-drawLandmarks3D(ax, front_preds, label="Front",  cPoint=(0,1,0), cLine=(0.2,0.7,0.2))
+#drawLandmarks3D(ax, P, label="LR",  cPoint=(1,1,0), cLine=(0.7,0.5,0.7))
+#drawLandmarks3D(ax, front_preds, label="Front",  cPoint=(0,1,0), cLine=(0.2,0.7,0.2))
+
+i = 0 
+ax.scatter(left_preds[i, 0], left_preds[i, 1],left_preds[i, 2], color=(0,1,1))
+ax.scatter(right_preds[i, 0], right_preds[i, 1],right_preds[i, 2],color=(0,1,0))
 
 labelMaker(ax)
 ax.set_xlabel("X")
--- transforms.py
+++ transforms.py
@@ -34,27 +34,28 @@ def transformationMat(rot, trans):
 def coincideLandmarkLR(LP, RP):
     nTop = 27 # index of the top point on nose
     nTip = 29 # index of tip of the nose in predictions
-    dx, dy, _ = RP[nTip] - LP[nTip] 
+    dx, dy, dz = RP[nTip] - LP[nTip] 
 
     RP[:, 0] -= dx
     RP[:, 1] -= dy
-
-    # rotate RP to coincide better with LP
-    vR = RP[nTop] - RP[nTip]
-    vL = LP[nTop] - LP[nTip]
-    vR[2] = 0
-    vL[2] = 0
-    R = rotationMatrix(vR, vL)
-
-    Trot = transformationMat(R, np.zeros((3,1)))
-    Ttrans = transformationMat(np.eye(3) , np.expand_dims(RP[nTip].T, axis=1)) 
-    nTtrans = transformationMat(np.eye(3) , -np.expand_dims(RP[nTip].T, axis=1)) 
-
-    RPT = np.hstack((RP , np.ones((RP.shape[0], 1))))
-
-    #RP = ((Ttrans @ Trot @ nTtrans) @ RPT.T)[:3, :].T
-    RP = ( RPT @ (Ttrans @ Trot @ nTtrans).T)[:, :3]
-   
+    RP[:, 2] -= dz
+#
+#    # rotate RP to coincide better with LP
+#    vR = RP[nTop] - RP[nTip]
+#    vL = LP[nTop] - LP[nTip]
+#    vR[2] = 0
+#    vL[2] = 0
+#    R = rotationMatrix(vR, vL)
+#
+#    Trot = transformationMat(R, np.zeros((3,1)))
+#    Ttrans = transformationMat(np.eye(3) , np.expand_dims(RP[nTip].T, axis=1)) 
+#    nTtrans = transformationMat(np.eye(3) , -np.expand_dims(RP[nTip].T, axis=1)) 
+#
+#    RPT = np.hstack((RP , np.ones((RP.shape[0], 1))))
+#
+#    #RP = ((Ttrans @ Trot @ nTtrans) @ RPT.T)[:3, :].T
+#    RP = ( RPT @ (Ttrans @ Trot @ nTtrans).T)[:, :3]
+#   
     return RP
 
 # coincides front_preds(RP) and profile_preds(LP) using tip of the nose
@@ -68,3 +69,56 @@ def coincideLandmarkFP(FP, PP):
     FP[:, 2] -= dz
 
     return FP
+
+def mergeLR(LP, RP):
+    merged = []
+    # jaw
+    merged[:9] = RP[:9]
+    merged[9:17] = LP[7::-1]
+    merged[8] = (merged[8] + LP[8]) / 2 # middle point
+
+    # eyebrow
+    merged[17:22] = RP[17:22]
+    merged[22:27] = LP[17:22]
+
+    # nose
+    merged[27:31] = (RP[27:31] + LP[27:31]) / 2  
+
+    # under nose
+    merged[31:34] = RP[31:34]
+    merged[34:36] = LP[32:30:-1]
+    merged[33] = (merged[33] + LP[33]) / 2 # middle point
+
+    # eye
+    merged[36:42] = RP[36:42]
+    merged[42:48] = LP[36:42]
+
+    # lips
+    merged[48:68] = RP[48:68]
+    merged = np.asarray(merged)
+    i = [52, 53, 54, 55, 56, 63, 64, 65]
+    j = [50, 49, 48, 59, 58, 61, 60, 67]
+    merged[j] = LP[i]
+    i = [51, 57, 62, 66]
+    merged[i] = (merged[i] + LP[i]) / 2
+
+    return merged
+
+def megeLandmarks(LP, RP, FP):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
