2c9082cce847b7f1328830647e6ca71d1881c287
redhood <a.tabrizi.mech@gmail.com>
2020-03-24 19:24:29 +0430

    created a pipeline for extracting landmarks

--- detect_landmarks.py
+++ detect_landmarks.py
@@ -3,64 +3,45 @@ from skimage import io
 from plot_landmarks import *
 import numpy as np 
 from transforms import *
-
+import os.path
+    
 # Run the 3D face alignment on a test image, without CUDA.
 FA = face_alignment.FaceAlignment(face_alignment.LandmarksType._3D, device='cpu', flip_input=False)
+BASE_DIR = "post/"
+for i in range(1,377):
+    print("Processing Case #", i)
 
-left_img = io.imread('post/Case#2_3_L_post_Pa_Ro.png')
-right_img = io.imread('post/Case#2_2_R_post_Pa_Ro.png')
-front_img = io.imread('post/Case#2_1_F_post_Pa_Ro_Sc_Tr_Ep.png')
+    if i in [35, 36]:
+        print("excluding current image due to errors!")
+        continue
 
-left_preds = np.asarray(FA.get_landmarks(left_img)[-1])
-right_preds = np.asarray(FA.get_landmarks(right_img)[-1])
-front_preds =  np.asarray(FA.get_landmarks(front_img)[-1])    
+    SAVE_DIR = BASE_DIR + "data/landmarks#" + str(i) + ".npy"
+    if os.path.exists(SAVE_DIR):
+        print("Data file already exists, moving on!")
+        continue
 
-print("PLOTTING")
-#fig = plt.figure()
-#ax = plt.gca()
-#drawLandmarksOnFace(ax, left_img, left_preds, color=(1,0,0))
-#fig = plt.figure()
-#ax = plt.gca()
-#drawLandmarksOnFace(ax, right_img, right_preds, color=(1,0,0))
-#
-right_preds = flip(right_preds, right_img.shape[1], 0)
-right_preds = coincideLandmarkLR(left_preds, right_preds)
-#
-#fig = plt.figure()
-#ax = plt.axes()
-#drawLandmarks2D(ax, left_preds, color=(0,0,1))
-#drawLandmarks2D(ax, right_preds, color=(1,0,0))
-##i = 27
-##ax.plot(left_preds[i, 0], left_preds[i, 1],color=(0,1,0), **plot_style)
-#ax.set_xlim([0, right_img.shape[1]])
-#ax.set_ylim([0, right_img.shape[0]])
-#ax.set_aspect('equal')
-#ax.invert_yaxis()
+    left_img = io.imread(BASE_DIR + 'Case#'+ str(i) + '_3_L_post.png')[:, :, :3]
+    right_img = io.imread(BASE_DIR + 'Case#'+ str(i) + '_2_R_post.png')[:, :, :3]
+    front_img = io.imread(BASE_DIR + 'Case#'+ str(i) + '_1_F_post.png')[:, :, :3]
 
-# swap x and z axes to be same with frontal axes
-left_preds[:, [0, 2]] = left_preds[:, [2, 0]]
-right_preds[:, [0, 2]] = right_preds[:, [2, 0]]
+    try:
+        left_preds = np.asarray(FA.get_landmarks(left_img)[-1])
+        right_preds = np.asarray(FA.get_landmarks(right_img)[-1])
+    except:
+        print("excluding current image due to errors!")
+        continue
+    front_preds =  np.asarray(FA.get_landmarks(front_img)[-1])    
 
-front_preds = flip(front_preds, np.max(front_preds[:,2]), 2)
-front_preds = coincideLandmarkFP(front_preds, left_preds)
+    right_preds = flip(right_preds, right_img.shape[1], 0)
+    right_preds = coincideLandmarkLR(left_preds, right_preds)
 
-P = mergeLR(left_preds, right_preds)
-LRFP = mergeLandmarks(left_preds, right_preds, front_preds)
+    # swap x and z axes to be same with frontal axes
+    left_preds[:, [0, 2]] = left_preds[:, [2, 0]]
+    right_preds[:, [0, 2]] = right_preds[:, [2, 0]]
 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-#drawLandmarks3D(ax, left_preds, label="Left")
-#drawLandmarks3D(ax, right_preds, label="Right", cPoint=(1,0,0), cLine=(0.7,0.2,0.2))
-#drawLandmarks3D(ax, P, label="LR",  cPoint=(1,1,0), cLine=(0.7,0.5,0.7))
-drawLandmarks3D(ax, LRFP, label="LRFP")
-#drawLandmarks3D(ax, front_preds, label="Front",  cPoint=(0,1,0), cLine=(0.2,0.7,0.2))
+    front_preds = flip(front_preds, np.max(front_preds[:,2]), 2)
+    front_preds = coincideLandmarkFP(front_preds, left_preds)
 
-i = 0 
-ax.scatter(left_preds[i, 0], left_preds[i, 1],left_preds[i, 2], color=(0,1,1))
-ax.scatter(right_preds[i, 0], right_preds[i, 1],right_preds[i, 2],color=(0,1,0))
+    LRFP = mergeLandmarks(left_preds, right_preds, front_preds)
 
-labelMaker(ax)
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("Z")
-plt.show()
+    np.save(SAVE_DIR, LRFP)
